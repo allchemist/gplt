6da30365b775862247d256012ce57822b9c137f2
allchemist <hohlovivan@gmail.com>
2010-09-04 00:07:40 +0400

    make-plot function

--- gplt.lisp
+++ gplt.lisp
@@ -1,4 +1,13 @@
-(defpackage :gplt (:use :cl))
+(defpackage :gplt (:use :cl)
+  (:export :*gnuplot-process*
+	   :*gnuplot-input*
+	   :*gnuplot-output*
+	   :gplt-start
+	   :gplt-stop
+	   :gplt-restart
+	   :gplt-display
+	   :gplt-exec
+	   :make-plot))
 
 (in-package :gplt)
 
@@ -15,7 +24,7 @@
     (values (sb-ext:process-input proc) (sb-ext:process-output proc) proc)))
 
 ;; functions to convert lisp clauses into infix form
-
+ 
 (defun insert-op (op args)
   (let ((clause (list (first args))))
     (dolist (arg (rest args))
@@ -44,41 +53,6 @@
 (defun infix-to-string (infix)
   (string-downcase (format nil "~A" infix)))
 
-;; gnuplot ops
-
-(defun set-param (fd param val)
-  (format fd "set ~A ~A~%" (string-downcase (string param)) val))
-
-(defun unset-param (fd param)
-  (format fd "unset ~A~%" (string-downcase (string param))))
-
-;; simple gnuplot call
-#|
-(defun gplt (&key func file (params "with lines") (sets '((term "x11"))) (unsets '(key)))
-  (let ((fd (open-pipe)))
-    (progn
-      (dolist (k sets)
-	 (set-param fd (first k) (second k)))
-      (dolist (k unsets)
-	(unset-param fd k))
-      (cond ((and (null func) (null file))
-	     (error "function or data file needed"))
-	    ((and (not (null func)) (not (null file)))
-	     (error "don't set both function and data"))
-	    ((not (null file))
-	     (let ((dim 0))
-	       (with-open-file (s file)
-		 (let ((str (make-string-input-stream (read-line s))))
-		   (loop (if (null (read str)) (return) (incf dim)))))
-	       (format fd (cond ((< dim 3) "plot ~A ~A~%" file params)
-				((= dim 3) "splot ~A ~A~%" file params)
-				(t (error "can't plot dimensions more than 3, check data file"))))))
-	    ((not (null func))
-	     (let ((func-str (infix-to-string (parse-prefix func))))
-	       (format fd (if (find #\y func-str) "splot ~A ~A~%" "plot ~A ~A~%") func-str params))))
-       (close fd))))
-|#
-
 ;; interactive mode
 
 (defvar *gnuplot-input*)
@@ -99,19 +73,20 @@
   (mapcar #'(lambda (c) (if (listp c) (convert-clause c) c)) cmd))
 
 (defun gplt-exec (cmd)
-  (let ((clause
-	 (case (first cmd)
-	   ((or plot splot)
-	      (append (list (first cmd)
-			    (if (stringp (second cmd))
-				(second cmd)
-				(parse-prefix (second cmd))))
-		      (parse-cmd (cddr cmd))))
-	   (t (parse-cmd cmd)))))
-    (format *gnuplot-input* "~A"
-	    (string-trim "()" (string-downcase (format nil "~A~%" clause))))
-    ))
-;    (format *query-io* "~A~%" (read-line *gnuplot-output*))))
+  (when (not (null cmd))
+    (let ((clause
+	   (case (first cmd)
+	     ((or plot splot)
+		(append (list (first cmd)
+			      (if (stringp (second cmd))
+				  (second cmd)
+				  (parse-prefix (second cmd))))
+			(parse-cmd (cddr cmd))))
+	     (t (parse-cmd cmd)))))
+      (format *gnuplot-input* "~A"
+	      (string-trim "()" (string-downcase (format nil "~A~%" clause))))
+      )))
+      ;    (format *query-io* "~A~%" (read-line *gnuplot-output*))))
       
 (defun gplt-display ()
   (close *gnuplot-input*)
@@ -130,3 +105,61 @@
   (gplt-exec (read stream t nil t)))
 
 (set-dispatch-macro-character #\# #\G #'sharp-G)
+
+;; plotting functions
+
+(defun array-row (array row)
+  (let ((row-list nil))
+    (dotimes (i (array-dimension array 1))
+      (push (aref array row i) row-list))
+    (nreverse row-list)))
+
+;; single plot
+
+;; comments at some keys:
+;; labels: (xlabel ylabel zlabel), when {x,y,z}label is nil - no label
+;; ranges: (xrange yrange zrange)
+;; draw-with: lines, points, pm3d, etc
+;; other-commands: args for gplt-exec
+
+;; data may be function (s-clause) or array nx2 or nx3
+;; if s-clause contains symbol 'y' or array has 3 columns,
+;; then use 'splot', else 'plot'
+
+(defun make-plot (data &key title ranges labels draw-with color pointsize pointtype terminal output other-commands)
+  (gplt-restart)
+  (map nil #'gplt-exec
+       (remove nil `(,(when title `(set title ,title))
+		      (unset key)
+		      ,(when terminal `(set term ,terminal))
+		      ,(when output `(set output ,(write-to-string output)))
+		      ,(when (first labels) `(set xlabel ,(write-to-string (first labels))))
+		      ,(when (second labels) `(set ylabel ,(write-to-string (second labels))))
+		      ,(when (third labels) `(set zlabel ,(write-to-string (third labels))))
+		      ,(when (first ranges) `(set xrange ,(cons 'range (first ranges))))
+		      ,(when (second ranges) `(set yrange ,(cons 'range (second ranges))))
+		      ,(when (third ranges) `(set zrange ,(cons 'range (third ranges)))))))
+  (map nil #'gplt-exec other-commands)
+  (let ((plot-params 
+	 (remove nil
+		 (append 
+		  (when draw-with `(with ,draw-with))
+		  (when color `(lc rgb ,(format nil "'~A'" color)))
+		  (when pointtype `(pt ,pointtype))
+		  (when pointsize `(ps ,pointsize))))))
+    (cond ((listp data)
+	   (gplt-exec
+	    (print (append `(,(if (find #\y (string-downcase (write-to-string data)))
+				  'splot 'plot)
+			      ,data)
+			   plot-params))))
+	  ((arrayp data)
+	   (gplt-exec
+	    (append `(,(ecase (array-dimension data 1) (2 'plot) (3 'splot))
+		       "'-'")
+		    plot-params))
+	   (dotimes (i (array-dimension data 0))
+	     (gplt-exec (array-row data i)))
+	   (gplt-exec '(e)))))
+  (gplt-display))
+
